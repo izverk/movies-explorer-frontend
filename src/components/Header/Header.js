060803291f1f0fa80b0677be2dbaf1298d52ad75
Ilya Zverkov <izverk78@yandex.ru>
2022-04-06 10:35:04 +0300

    refactor: оптимизирована верстка шапки, проверено в PerfectPixel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,34 +11,18 @@ function Header({ loggedIn }) {
     <header className={`header app__wide-section${loggedIn ? ' header_background_no' : ''}`}>
       <Logo />
       {!loggedIn ? (
-        <ul className='header__reg-links-container'>
-
-          <li>
-            <Link to='/signup' className='header__signup-link app__link app__link_color_white'>Регистрация</Link>
-          </li>
-
-          <li>
-            <Link to='/signin' className='header__signin-link app__link'>Войти</Link>
-          </li>
-
-        </ul>
+        <div className='header__reg-links'>
+          <Link to='/signup' className='app__link app__link_color_white header__signup-link'>Регистрация</Link>
+          <Link to='/signin' className='app__link header__signin-link'>Войти</Link>
+        </div>
       ) : (
-        <div className='header__navigation-container'>
-
-          <ul className='header__movies-links-list'>
-            <li className='header__movies-links-item'>
-              <NavLink to='/movies' className='app__link header__movies-link' activeClassName='header__movies-link_active'>
-                Фильмы
-              </NavLink>
-            </li>
-            <li className='header__movies-links-item'>
-              <NavLink to='/saved-movies' className='app__link header__movies-link' activeClassName='header__movies-link_active'>
-                Сохраненные фильмы
-              </NavLink>
-            </li>
-          </ul>
-
-
+        <nav className='header__nav-links'>
+          <NavLink to='/movies' className='app__link header__nav-link' activeClassName='header__nav-link_active'>
+            Фильмы
+          </NavLink>
+          <NavLink to='/saved-movies' className='app__link header__nav-link' activeClassName='header__nav-link_active'>
+            Сохраненные фильмы
+          </NavLink>
           <Link to='/profile' className='app__link header__profile-link' title='Переход на страницу "Профиль пользователя"'>
             <span className='header__profile-link-text'>
               Аккаунт
@@ -49,7 +33,34 @@ function Header({ loggedIn }) {
           <button className='app__link header__menu-button' type='button'>
             <img className='header__menu-button-icon' src={menuIcon} alt='Кнопка профиля пользователя' title='Переход на страницу "Профиль пользователя"' />
           </button>
-        </div>
+        </nav>
+        // <div className='header__navigation-container'>
+
+        //   <ul className='header__movies-links-list'>
+        //     <li className='header__movies-links-item'>
+        //       <NavLink to='/movies' className='app__link header__movies-link' activeClassName='header__movies-link_active'>
+        //         Фильмы
+        //       </NavLink>
+        //     </li>
+        //     <li className='header__movies-links-item'>
+        //       <NavLink to='/saved-movies' className='app__link header__movies-link' activeClassName='header__movies-link_active'>
+        //         Сохраненные фильмы
+        //       </NavLink>
+        //     </li>
+        //   </ul>
+
+
+        //   <Link to='/profile' className='app__link header__profile-link' title='Переход на страницу "Профиль пользователя"'>
+        //     <span className='header__profile-link-text'>
+        //       Аккаунт
+        //     </span>
+        //     <img className='header__profile-icon' src={profileIcon} alt='Иконка профиля пользователя' />
+        //   </Link>
+
+        //   <button className='app__link header__menu-button' type='button'>
+        //     <img className='header__menu-button-icon' src={menuIcon} alt='Кнопка профиля пользователя' title='Переход на страницу "Профиль пользователя"' />
+        //   </button>
+        // </div>
       )
       }
     </header >
