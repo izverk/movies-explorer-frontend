c53dee248182fc77937e7186a285078bf046a886
Ilya Zverkov <izverk78@yandex.ru>
2022-04-06 17:49:33 +0300

    fix: скорректирован тег div на section в комп-х Main и Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,14 +9,14 @@ import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
     return (
-        <div className='main'>
+        <main className='main'>
             <Promo />
             <NavTab />
             <AboutProject />
             <Techs />
             <AboutMe />
             <Portfolio />
-        </div>
+        </main>
     );
 }
 
