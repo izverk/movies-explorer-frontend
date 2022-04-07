3f78db8dfa5b7898cdff434b95a6a9f1c8b1adb9
Ilya Zverkov <izverk78@yandex.ru>
2022-04-07 11:35:20 +0300

    feat: сверстаны ком-ты AboutProject и SectionTitle с адаптивом

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './AboutProject.css';
+import Divider from '../SectionTitle/SectionTitle';
 
 function AboutProject() {
     return (
-        <div className="AboutProject" id='AboutProject'>
-            О проекте
-        </div>
+        <section className="about-project" id='about-project'>
+            <Divider>
+                О проекте
+            </Divider>
+            <ul className='about-project__columns'>
+                <li className='about-project__column'>
+                    <h3 className='about-project__column-title'>
+                        Дипломный проект включал 5 этапов
+                    </h3>
+                    <p className='about-project__column-text'>
+                        Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+                    </p>
+                </li>
+                <li className='about-project__column'>
+                    <h3 className='about-project__column-title'>
+                        На выполнение диплома ушло 5 недель
+                    </h3>
+                    <p className='about-project__column-text'>
+                        У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+                    </p>
+                </li>
+            </ul>
+            <ul className='about-project__weeks'>
+                <li className='about-project__weeks-column about-project__weeks-column_type_narrow'>
+                    <h3 className='about-project__weeks-column-title about-project__weeks-column-title_type_narrow'>
+                        1 неделя
+                    </h3>
+                    <p className='about-project__weeks-column-text'>
+                        Back-end
+                    </p>
+                </li>
+                <li className='about-project__weeks-column about-project__weeks-column_type_wide'>
+                    <h3 className='about-project__weeks-column-title about-project__weeks-column-title_type_wide'>
+                        4 недели
+                    </h3>
+                    <p className='about-project__weeks-column-text'>
+                        Front-end
+                    </p>
+                </li>
+            </ul>
+
+        </section>
     );
 }
 
