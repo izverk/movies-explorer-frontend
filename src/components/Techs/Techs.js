0b07711f92f442ed67edae0088a279b685a40350
Ilya Zverkov <izverk78@yandex.ru>
2022-04-07 18:33:58 +0300

    feat: сверстан компонент Techs, проверено в PerfectPixel

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './Techs.css';
-import Divider from '../SectionTitle/SectionTitle';
+import SectionTitle from '../SectionTitle/SectionTitle';
 
 function Techs() {
     return (
         <section className='techs' id='techs'>
-            <Divider>
+            <SectionTitle>
                 Технологии
-            </Divider>
+            </SectionTitle>
+            <h3 className='techs__subtitle'>
+                7 технологий
+            </h3>
+            <p className='techs__description'>
+                На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+            </p>
+            <ul className='techs__list'>
+                <li className='techs__list-item'>
+                    HTML
+                </li>
+                <li className='techs__list-item'>
+                    CSS
+                </li>
+                <li className='techs__list-item'>
+                    JS
+                </li>
+                <li className='techs__list-item'>
+                    React
+                </li>
+                <li className='techs__list-item'>
+                    Git
+                </li>
+                <li className='techs__list-item'>
+                    Express.js
+                </li>
+                <li className='techs__list-item'>
+                    mongoDB
+                </li>
+            </ul>
         </section>
     );
 }
