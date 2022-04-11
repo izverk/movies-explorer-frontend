8d393244bf5cd0ac674c45228215a6f1e6f2942f
Ilya Zverkov <izverk78@yandex.ru>
2022-04-11 11:50:33 +0300

    refactor: закомментирован резервный массив в constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,18 @@ export const movies = [
   { id: 11, nameRU: "Пи Джей Харви: A dog called money", duration: '1ч 4м', image: '../../images/pic__COLOR_pic(11).jpg', isLiked: false },
   { id: 12, nameRU: "По волнам: Искусство звука в кино", duration: '1ч 7м', image: '../../images/pic__COLOR_pic(12).jpg', isLiked: false }
 ]
+
+// export const movies = [
+//   { id: 1, nameRU: "33 слова о дизайне", duration: '1ч 47м', image: '../../images/pic__COLOR_pic(1).jpg', isLiked: true },
+//   { id: 2, nameRU: "Киноальманах «100 лет дизайна»", duration: '1ч 3м', image: '../../images/pic__COLOR_pic(2).jpg', isLiked: false },
+//   { id: 3, nameRU: "В погоне за Бенкси", duration: '1ч 42м', image: '../../images/pic__COLOR_pic(3).jpg', isLiked: false },
+//   { id: 4, nameRU: "Баския: Взрыв реальности", duration: '1ч 21м', image: '../../images/pic__COLOR_pic(4).jpg', isLiked: false },
+//   { id: 5, nameRU: "Бег это свобода", duration: '1ч 44м', image: '../../images/pic__COLOR_pic(5).jpg', isLiked: false },
+//   { id: 6, nameRU: "Книготорговцы", duration: '1ч 37м', image: '../../images/pic__COLOR_pic(6).jpg', isLiked: true },
+//   { id: 7, nameRU: "Когда я думаю о Германии ночью", duration: '1ч 56м', image: '../../images/pic__COLOR_pic(7).jpg', isLiked: false },
+//   { id: 8, nameRU: "Gimme Danger: История Игги и The Stooge...", duration: '1ч 59м', image: '../../images/pic__COLOR_pic(8).jpg', isLiked: true },
+//   { id: 9, nameRU: "Дженис: Маленькая девочка грустит", duration: '1ч 42м', image: '../../images/pic__COLOR_pic(9).jpg', isLiked: true },
+//   { id: 10, nameRU: "Соберись перед прыжком", duration: '1ч 10м', image: '../../images/pic__COLOR_pic(10).jpg', isLiked: false },
+//   { id: 11, nameRU: "Пи Джей Харви: A dog called money", duration: '1ч 4м', image: '../../images/pic__COLOR_pic(11).jpg', isLiked: false },
+//   { id: 12, nameRU: "По волнам: Искусство звука в кино", duration: '1ч 7м', image: '../../images/pic__COLOR_pic(12).jpg', isLiked: false }
+// ]
