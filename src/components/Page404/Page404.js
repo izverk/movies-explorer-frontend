c9963eda46a373dfb52596b35f25977a821a1be3
Ilya Zverkov <izverk78@yandex.ru>
2022-04-13 14:15:25 +0300

    feat: сверстана страница 404

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -6,9 +6,19 @@ function Page404() {
     const history = useHistory();
 
     return (
-        <div className='Page404'>
-            Страница не найдена
-            <button to='/signin' onClick={() => { history.goBack() }}>Назад</button>
+        <div className='page404'>
+            <h1 className='page404__title'>
+                404
+            </h1>
+            <p className='page404__text'>
+                Страница не найдена
+            </p>
+            <button
+                to='/signin'
+                onClick={() => { history.goBack() }}
+                className='app__link page404__button'>
+                Назад
+            </button>
         </div>
     );
 }
