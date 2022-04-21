b2384b9d64a9668cb1099cc6fef1a488c1ed2704
Ilya Zverkov <izverk78@yandex.ru>
2022-04-21 12:47:49 +0300

    feat: реализ независимая работа чек-бокса короткометражек

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import './FilterCheckbox.css';
+import { filterWithDuration } from '../../utils/utils';
+import { nothingFoundMessageText } from '../../utils/constants';
 
-function FilterCheckbox({
-	shortFilmsCheckboxValue,
-	setShortFilmsCheckboxValue,
-}) {
-	const handleInputChange = (e) => {
+function FilterCheckbox() {
+	const context = React.useContext(CurrentUserContext);
+	const {
+		movies,
+		setShortMovies,
+		shortFilmsCheckboxValue,
+		setShortFilmsCheckboxValue,
+		setBadSearchResult,
+	} = context;
+
+	// Обработчик изменения значения чек-бокса короткометражек
+	const handleChange = (e) => {
 		setShortFilmsCheckboxValue(e.target.checked);
 	};
+	// Функция включения/выключения фильтра короткометражек
+	const filterShortFilms = useCallback(() => {
+		if (shortFilmsCheckboxValue) {
+			const shortFilms = filterWithDuration(movies);
+			if (shortFilms.length) {
+				setShortMovies(shortFilms);
+			} else {
+				setBadSearchResult(nothingFoundMessageText);
+			}
+		} else {
+			setBadSearchResult(null);
+		}
+	}, [movies, shortFilmsCheckboxValue, setShortMovies, setBadSearchResult]);
+
+	React.useEffect(() => {
+		filterShortFilms();
+	}, [shortFilmsCheckboxValue, filterShortFilms]);
 
 	return (
 		<label className='filter'>
@@ -16,7 +43,7 @@ function FilterCheckbox({
 				checked={shortFilmsCheckboxValue}
 				className='filter__checkbox'
 				name='filterCheckbox'
-				onChange={handleInputChange}
+				onChange={handleChange}
 			/>
 			<span className='filter__pseudo-checkbox' />
 			Короткометражки
